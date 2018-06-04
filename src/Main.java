8c718cc8cb7d8d0e2e0d93653ccdcb78d2df219d
Jarek <jjankowski1@gmail.com>
2018-06-04 17:45:37 +0200

    małe poprawki

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
 
     public static void main(String[] args) {
-        int inSilnia = 7;
+        int inSilnia = 5;
         System.out.println(Silnia1(inSilnia));
         System.out.println(Silnia2(inSilnia));
         System.out.println(SilniaReq(inSilnia));
@@ -18,16 +18,16 @@ public class Main {
     }
 
     public static long Silnia2(int n){ // pętla z dołu
-        long silnia = n;
-        for (int i = 1; i < n; i++) {
-            silnia = silnia * i;
+        long silnia = 1;
+        for (int i = 1; i <= n; i++) {
+            silnia *= i;
         }
         return silnia;
     }
 
     private static long SilniaReq(int i) //rekurencyjnie
     {
-        if (i < 1)
+        if (i == 1)
             return 1;
         else
             return i * SilniaReq(i - 1);

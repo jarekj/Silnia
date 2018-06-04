ad6ceb7e4b9ee6015c0c5d8cec1a992cfd192925
Jarek <jjankowski1@gmail.com>
2018-06-04 15:25:33 +0200

    first commit

---
+++ src/Main.java
@@ -0,0 +1,37 @@
+public class Main {
+
+    public static void main(String[] args) {
+        int inSilnia = 7;
+        System.out.println(Silnia1(inSilnia));
+        System.out.println(Silnia2(inSilnia));
+        System.out.println(SilniaReq(inSilnia));
+    }
+
+
+    public static long Silnia1(int n) { // pętla z góry
+        long silnia = n;
+        for (int i = n-1; i > 0; i--) {
+                 silnia = silnia * i;
+        }
+        return silnia;
+
+    }
+
+    public static long Silnia2(int n){ // pętla z dołu
+        long silnia = n;
+        for (int i = 1; i < n; i++) {
+            silnia = silnia * i;
+        }
+        return silnia;
+    }
+
+    private static long SilniaReq(int i) //rekurencyjnie
+    {
+        if (i < 1)
+            return 1;
+        else
+            return i * SilniaReq(i - 1);
+    }
+
+
+}
